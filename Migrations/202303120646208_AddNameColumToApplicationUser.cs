72268a0466d2e3506c8170afa4e7e10576c09794
84917 <84917@LAPTOP-1LMJI7FF>
2023-03-12 14:33:15 +0700

    Add project files.

---
+++ Migrations/202303120646208_AddNameColumToApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿namespace BigSchool_LyThuyet.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddNameColumToApplicationUser : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "Name", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "Name");
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BigSchool_LyThuyet.Startup))]
+namespace BigSchool_LyThuyet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
